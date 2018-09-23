dd249c106872bc97ad2007f9d197a4a8782b88f2
aha-oretama <sekine_y_529@msn.com>
2018-09-24 03:33:41 +0900

    Refactor: rename test cases.

--- src/test/java/jp/aha/oretama/typoFixer/service/TypoCheckerServiceTest.java
+++ src/test/java/jp/aha/oretama/typoFixer/service/TypoCheckerServiceTest.java
@@ -19,7 +19,7 @@ public class TypoCheckerServiceTest {
 
     private TypoCheckerService service = new TypoCheckerService(new JLanguageTool(new AmericanEnglish()), null);
 
-    private String rawDiff = "diff --git a/README.md b/README.md\n" +
+    private String oneLineRawDiff = "diff --git a/README.md b/README.md\n" +
             "index f247396..93a9c32 100644\n" +
             "--- a/README.md\n" +
             "+++ b/README.md\n" +
@@ -28,7 +28,30 @@ public class TypoCheckerServiceTest {
             "+Nobody reads\n" +
             "\\ No newline at end of file";
 
-    private String rawDiff2 = "diff --git a/typo-fixer.json b/typo-fixer.json\n" +
+    private String afterIsOneLineRawDiff = "diff --git a/README.md b/README.md\n" +
+            "index 3398d7a..660bb3a 100644\n" +
+            "--- a/README.md\n" +
+            "+++ b/README.md\n" +
+            "@@ -1,2 +1 @@\n" +
+            "-# test\n" +
+            "-aaa\n" +
+            "+# test3\n" +
+            "\\ No newline at end of file";
+
+    private String multiLineRawDiff = "diff --git a/README.md b/README.md\n" +
+            "index 8cacdfa..e41e46f 100644\n" +
+            "--- a/README.md\n" +
+            "+++ b/README.md\n" +
+            "@@ -1,4 +1,5 @@\n" +
+            " # test\n" +
+            " \n" +
+            "-This is test repository.\n" +
+            "-Ignore this repostiosy.\n" +
+            "+This is test for typo fixer.\n" +
+            "+This is typooo.\n" +
+            "+This is alsonn typo.";
+
+    private String twoFileRawDiff = "diff --git a/typo-fixer.json b/typo-fixer.json\n" +
             "new file mode 100644\n" +
             "index 0000000..f85f9be\n" +
             "--- /dev/null\n" +
@@ -50,32 +73,9 @@ public class TypoCheckerServiceTest {
             "+This is typooo.\n" +
             "+This is alsonn typo.";
 
-    private String rawDiff3 = "diff --git a/README.md b/README.md\n" +
-            "index 8cacdfa..e41e46f 100644\n" +
-            "--- a/README.md\n" +
-            "+++ b/README.md\n" +
-            "@@ -1,4 +1,5 @@\n" +
-            " # test\n" +
-            " \n" +
-            "-This is test repository.\n" +
-            "-Ignore this repostiosy.\n" +
-            "+This is test for typo fixer.\n" +
-            "+This is typooo.\n" +
-            "+This is alsonn typo.";
-
-    private String rawDiff4 = "diff --git a/README.md b/README.md\n" +
-            "index 3398d7a..660bb3a 100644\n" +
-            "--- a/README.md\n" +
-            "+++ b/README.md\n" +
-            "@@ -1,2 +1 @@\n" +
-            "-# test\n" +
-            "-aaa\n" +
-            "+# test3\n" +
-            "\\ No newline at end of file";
-
     @Test
-    public void getSuggestionsFromRawDiff() {
-        List<Diff> result = service.getAdded(rawDiff);
+    public void getAddedFromOnlyOneLine() {
+        List<Diff> result = service.getAdded(oneLineRawDiff);
 
         assertEquals(1, result.size());
         Diff diff = result.get(0);
@@ -88,8 +88,38 @@ public class TypoCheckerServiceTest {
     }
 
     @Test
-    public void getSuggestionsFromRawDiff2() {
-        List<Diff> result = service.getAdded(rawDiff2);
+    public void getAddedFromOnlyOneLineInAfterFile() {
+        List<Diff> result = service.getAdded(afterIsOneLineRawDiff);
+
+        assertEquals(1, result.size());
+        Diff diff = result.get(0);
+        assertEquals("README.md", diff.getPath());
+
+        assertEquals(1, diff.getAddLines().size());
+        Diff.AddLine addLine = diff.getAddLines().get(0);
+        assertEquals("# test3", addLine.getLineContent());
+        assertEquals(1, addLine.getFileLine().intValue());
+        assertEquals(3, addLine.getDiffLine().intValue());
+    }
+
+    @Test
+    public void getAddedFromMultiLine() {
+        List<Diff> result = service.getAdded(multiLineRawDiff);
+
+        assertEquals(1, result.size());
+        Diff diff = result.get(0);
+        assertEquals("README.md", diff.getPath());
+
+        assertEquals(3, diff.getAddLines().size());
+        Diff.AddLine addLine = diff.getAddLines().get(0);
+        assertEquals("This is test for typo fixer.", addLine.getLineContent());
+        assertEquals(3, addLine.getFileLine().intValue());
+        assertEquals(5, addLine.getDiffLine().intValue());
+    }
+
+    @Test
+    public void getAddedFromTwoFile() {
+        List<Diff> result = service.getAdded(twoFileRawDiff);
 
         assertEquals(2, result.size());
         Diff diff0 = result.get(0);
@@ -107,35 +137,5 @@ public class TypoCheckerServiceTest {
         assertEquals("This is test for typo fixer.", addLine1.getLineContent());
         assertEquals(3, addLine1.getFileLine().intValue());
         assertEquals(5, addLine1.getDiffLine().intValue());
-    }
-
-    @Test
-    public void getSuggestionsFromRawDiff3() {
-        List<Diff> result = service.getAdded(rawDiff3);
-
-        assertEquals(1, result.size());
-        Diff diff = result.get(0);
-        assertEquals("README.md", diff.getPath());
-
-        assertEquals(3, diff.getAddLines().size());
-        Diff.AddLine addLine = diff.getAddLines().get(0);
-        assertEquals("This is test for typo fixer.", addLine.getLineContent());
-        assertEquals(3, addLine.getFileLine().intValue());
-        assertEquals(5, addLine.getDiffLine().intValue());
-    }
-
-    @Test
-    public void getSuggestionsFromRawDiff4() {
-        List<Diff> result = service.getAdded(rawDiff4);
-
-        assertEquals(1, result.size());
-        Diff diff = result.get(0);
-        assertEquals("README.md", diff.getPath());
-
-        assertEquals(1, diff.getAddLines().size());
-        Diff.AddLine addLine = diff.getAddLines().get(0);
-        assertEquals("# test3", addLine.getLineContent());
-        assertEquals(1, addLine.getFileLine().intValue());
-        assertEquals(3, addLine.getDiffLine().intValue());
     }
 }
